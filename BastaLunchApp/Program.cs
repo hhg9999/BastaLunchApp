3a6579a7fbfdda997f47afd050c27b4382007298
hhg9999 <39940853+hhg9999@users.noreply.github.com>
2020-09-09 12:38:41 +0200

    Update Program.cs

--- BastaLunchApp/Program.cs
+++ BastaLunchApp/Program.cs
@@ -6,7 +6,7 @@ namespace BastaLunchApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!" + msg);
         }
     }
 }
