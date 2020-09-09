48b4beb3bf88968e36fa5e07684056ab245451c0
hhg9999 <39940853+hhg9999@users.noreply.github.com>
2020-09-09 12:41:01 +0200

    Update Program.cs

--- BastaLunchApp/Program.cs
+++ BastaLunchApp/Program.cs
@@ -6,7 +6,14 @@ namespace BastaLunchApp
     {
         static void Main(string[] args)
         {
+            string msg = "Anzahl Parameter" ;
             Console.WriteLine("Hello World!" + msg);
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(i.ToString());
+
+            }
+            Console.ReadKey();
         }
     }
 }
